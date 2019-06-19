6e1c6a60d74f5dced6fd538431e36343e557dc5e
Sepehr Samiei <sepehrs@amazon.com>
2019-06-19 15:01:49 +1000

    added WSUS basic configurations.

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # quickstart-microsoft-wsus
+
+TODO:
+
+1- Support standalone WSUS
+2- Support AD-joined WSUS
+3- Support SQL Server DB in addition to WID
+4- Support integration of WSUS configuration with SSM Patch Manager
+5- Support multiple languages
+6- Support other upstream servers
+7- Support SSL
+8- Support both Windows full and core
--- scripts/install-wsus.ps1
+++ scripts/install-wsus.ps1
@@ -1,4 +1,19 @@
-Install-WindowsFeature -Name UpdateServices -IncludeManagementTools
+param (
+    [Parameter(Mandatory=$true)][string] $langs,
+    [Parameter(Mandatory=$true)][string] $products,
+	[Parameter(Mandatory=$true)][string] $classifications,
+	[Parameter(Mandatory=$true)][int] $numOfSyncsPerDay
+ )
+ 
+ function CommaSeparatedListToStringCollection(string commaSeparatedList)
+ {
+	$strList = $commaSeparatedList.split(',');
+	$strCol = New-Object -TypeName "System.Collections.Specialized.StringCollection"
+	$strList.forEach{
+		$strCol.Add($_.trim())
+	}
+	return $strCol
+ }
 
 New-Item -Path D: -Name WSUS -ItemType Directory
 Set-Location -Path "C:\Program Files\Update Services\Tools"
@@ -13,49 +28,38 @@ $wsusConfig = $wsus.GetConfiguration()
 Set-WsusServerSynchronization –SyncFromMU
  
 #Set Update Languages to English and save configuration settings
-$wsusConfig.AllUpdateLanguagesEnabled = $false           
-$wsusConfig.SetEnabledUpdateLanguages(“en”)           
+$wsusConfig.AllUpdateLanguagesEnabled = $false
+$languages = CommaSeparatedListToStringCollection -commaSeparatedList $langs
+$wsusConfig.SetEnabledUpdateLanguages($languages)
 $wsusConfig.Save()
  
 #Get WSUS Subscription and perform initial synchronization to get latest categories
 $subscription = $wsus.GetSubscription()
 $subscription.StartSynchronizationForCategoryOnly()
  
-While ($subscription.GetSynchronizationStatus() -ne ‘NotProcessing’) {
-    Write-Host “.” -NoNewline
+While ($subscription.GetSynchronizationStatus() -ne 'NotProcessing') {
+    Write-Host "." -NoNewline
     Start-Sleep -Seconds 5
 }
-Write-Host “Sync is done.”
+Write-Host "Sync is done."
 
 #Configure the Platforms that we want WSUS to receive updates
+$productList = CommaSeparatedListToStringCollection -commaSeparatedList $products
 Get-WsusProduct | where-Object {
-    $_.Product.Title -in (
-    ‘CAPICOM’,
-    ‘Silverlight’,
-    ‘SQL Server 2008 R2’,
-    ‘SQL Server 2005’,
-    ‘SQL Server 2008’,
-    ‘Exchange Server 2010’,
-    ‘Windows Server 2003’,
-    ‘Windows Server 2008’,
-    ‘Windows Server 2008 R2’)
+    $_.Product.Title -in $productList
 } | Set-WsusProduct
  
 #Configure the Classifications
+$classificationsList = CommaSeparatedListToStringCollection -commaSeparatedList $classifications
 Get-WsusClassification | Where-Object {
-    $_.Classification.Title -in (
-    ‘Update Rollups’,
-    ‘Security Updates’,
-    ‘Critical Updates’,
-    ‘Service Packs’,
-    ‘Updates’)
+    $_.Classification.Title -in $classificationsList
 } | Set-WsusClassification
  
 #Configure Synchronizations
 $subscription.SynchronizeAutomatically=$true
 #Set synchronization scheduled for midnight each night
 $subscription.SynchronizeAutomaticallyTimeOfDay= (New-TimeSpan -Hours 0)
-$subscription.NumberOfSynchronizationsPerDay=1
+$subscription.NumberOfSynchronizationsPerDay = $numOfSyncsPerDay
 $subscription.Save()
  
 #Kick off a synchronization
