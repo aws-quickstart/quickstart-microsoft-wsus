a2569c610bf590b7780bdbf56f0d175860e7eb64
davmayd <davmayd@amazon.com>
2019-08-22 12:16:19 -0500

    Successfully launched

--- scripts/Install-WSUS.ps1
+++ scripts/Install-WSUS.ps1
@@ -1,4 +1,5 @@
 Install-WindowsFeature -Name UpdateServices -IncludeManagementTools
+
 New-Item -Path C: -Name WSUS -ItemType Directory
-cd "C:\Program Files\Update Services\Tools"
-.\wsusutil.exe postinstall CONTENT_DIR=C:\WSUS
+
+"C:\Program Files\Update Services\Tools.\wsusutil.exe" postinstall CONTENT_DIR=C:\WSUS
