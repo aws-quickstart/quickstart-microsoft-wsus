b99fec55817dc799e01f10314f2ed9fe91a00e89
Sepehr Samiei <sepehrs@amazon.com>
2019-10-05 20:49:24 +1000

    added parameter for volume size

--- scripts/approve-updates.ps1
+++ scripts/approve-updates.ps1
@@ -1,4 +1,36 @@
 $wsus = Get-WSUSServer
-$update = $wsus.SearchUpdates('Windows Server 2019')
+$sub = $wsus.GetSubscription()
+
+#Waiting for WSUS synchronization to complete
+do
+{
+	$syncStatus = $sub.GetSynchronizationStatus()
+	$sub.GetSynchronizationProgress()
+	Start-Sleep 60	
+} while ($syncStatus -eq "Running")
+Write-Host $syncStatus
+
+#Defining a target group for all computers
 $group = $wsus.GetComputerTargetGroups() | where {$_.Name -eq 'All Computers'}
-$update.forEach{$_.Approve("Install",$Group)}
+
+function ApproveUpdates($category)
+{
+	$update = $wsus.SearchUpdates($category)
+	$update.forEach{$_.Approve("Install",$Group)}
+}
+
+#Approving updates for a number of categories
+ApproveUpdates("Windows Server 2019")
+ApproveUpdates("Windows Server 2016")
+ApproveUpdates("Windows Server 2012 R2")
+
+#Checking WSUS content download progress
+$wsus.GetContentDownloadProgress()
+
+#Checking process CPU and memory
+while ($true)
+{
+	Clear-Host
+	Get-Process | Sort-Object WorkingSet64 -Descending | Select-Object Name,@{Name='WorkingSet';Expression={($_.WorkingSet64/1KB)}} -First 5
+	Start-Sleep 1
+}
