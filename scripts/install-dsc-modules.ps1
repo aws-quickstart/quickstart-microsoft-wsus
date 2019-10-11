c13c05fe90cb469ead94ce5aaf3c935925b7414c
Samiei <sepehrs@amazon.com>
2019-10-11 17:23:34 +1100

    cleaned up domain join scripts and removed redundant lines from workload yaml

--- scripts/install-dsc-modules.ps1
+++ scripts/install-dsc-modules.ps1
@@ -6,19 +6,15 @@ Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
 Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
 
 "Installing the needed Powershell DSC modules for this Quick Start"
-Install-Module -Name SqlServerDsc
 Install-Module -Name ComputerManagementDsc
-Install-Module -Name "xFailOverCluster"
 Install-Module -Name PSDscResources
-Install-Module -Name xSmbShare
 Install-Module -Name "xActiveDirectory"
-Install-Module SqlServer -Force -AllowClobber
 
 "Disabling Windows Firewall"
 Get-NetFirewallProfile | Set-NetFirewallProfile -Enabled False
 
 "Creating Directory for DSC Public Cert"
-New-Item -Path C:\AWSQuickstart\publickeys -ItemType directory -Force
+New-Item -Path C:\AWSQuickstart\publickeys -ItemType directory 
 
 "Setting up DSC Certificate to Encrypt Credentials in MOF File"
 $cert = New-SelfSignedCertificate -Type DocumentEncryptionCertLegacyCsp -DnsName 'AWSQSDscEncryptCert' -HashAlgorithm SHA256
